c6e91a0c1ccbe68e569c737660da56501479dfb2
omordkovich <o.mordkovich@hotmail.com>
2025-06-12 14:30:37 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Lesson01 from "./lessons_45FS/lessons/lesson_01/Lesson01.jsx";
+
+function App() {
+  return (
+    <>
+      <Lesson01 />
+      <Lesson01 />
+      <Lesson01 />
+    </>
+  );
+}
+
+export default App;
