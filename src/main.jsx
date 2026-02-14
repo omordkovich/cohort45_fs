7a6a39201c503aad76fd9dffd10f944299058974
omordkovich <o.mordkovich@hotmail.com>
2026-02-14 20:49:10 +0100

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import RootLayout from "./routes/RootLayout.jsx";
+import NewPost from "./components/post/NewPost.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
